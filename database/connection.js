aa87779ecdc6be44384be584f5ec66e6824733ec
Vansh Verma <137089594+vansh-code01@users.noreply.github.com>
2025-03-31 14:52:36 +0530

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ import mongoose, { mongo } from "mongoose";
 export const connection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "MERN_AUCTION_PLATFORM",
+    
     })
     .then(() => {
       console.log("Connected to Database.");
